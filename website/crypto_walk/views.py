de97408ab94848beaa42a92074ba8571662e2fd6
WernkeJD <Jacob.Wernke@yahoo.com>
2025-10-03 00:25:22 -0400

    testing

--- website/crypto_walk/views.py
+++ website/crypto_walk/views.py
@@ -80,8 +80,11 @@ def pick_crypto(request: HttpRequest):
 
                     crypto1 = json_data[random.randint(0, len(json_data))]["id"]
                     crypto2 = json_data[random.randint(0, len(json_data))]["id"]
-                    stock1 = Tickers.objects.get(id=(random.randint(42178, 45177))).ticker
-                    stock2 = Tickers.objects.get(id=(random.randint(42178, 45177))).ticker
+                    # stock1 = Tickers.objects.get(id=(random.randint(42178, 45177))).ticker
+                    # stock2 = Tickers.objects.get(id=(random.randint(42178, 45177))).ticker
+                    stock1 = Tickers.objects.get(id=(random.randint(1, 3000))).ticker
+                    stock2 = Tickers.objects.get(id=(random.randint(1, 3000))).ticker
+
                     
                     coin_prices = get_coin_price([crypto1, crypto2])
                     crypto_1_value = coin_prices[crypto1]["usd"]
